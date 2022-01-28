68a96c7fd8ef8355cbf7d2221866094364bde0b1
Jennika Wilson <j.r.a.112100@gmail.com>
2022-01-28 00:49:26 -0500

    added white and borders to colors

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ palette.addEventListener('click', function(e){
 let purple = document.createElement('div')
 purple.setAttribute('class', 'color-choice')
 purple.setAttribute('id', 'purple')
-purple.style.color = 'purple'
 purple.textContent = '■'
 
 palette.append(purple)
+
+let white = document.createElement('div')
+white.setAttribute('class', 'color-choice')
+white.setAttribute('id', 'white')
+white.textContent = '■'
+
+palette.append(white)
